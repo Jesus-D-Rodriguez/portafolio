165bb949e56d5f1b3f6d13a395c081fd6934694c
jesus <jdrch62@gmail.com>
2024-09-22 14:12:52 -0400

    a

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -102,9 +102,9 @@ function Footer() {
           <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
-        <Copyright>
+        {/* <Copyright>
           &copy; 2023 Rishav Chanda. All rights reserved.
-        </Copyright>
+        </Copyright> */}
 
       </FooterWrapper>
     </FooterContainer>
