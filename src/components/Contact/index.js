031e237feb73353a87d5209ccae397119acf18ac
jesus <jdrch62@gmail.com>
2024-09-22 13:59:46 -0400

    email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_r6zqj6b', 'template_foq77i8', form.current, 'th7Ej1witUjQ8GBVL')
       .then((result) => {
         setOpen(true);
         form.current.reset();
